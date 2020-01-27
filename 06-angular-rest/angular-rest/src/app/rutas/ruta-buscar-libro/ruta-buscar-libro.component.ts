31d9ab19a687b36d089d8c42f3855a30865a5061
Adrian Eguez <eadepto@hotmail.com>
2020-01-27 17:45:27 -0500

    router

--- 06-angular-rest/angular-rest/src/app/rutas/ruta-buscar-libro/ruta-buscar-libro.component.ts
+++ 06-angular-rest/angular-rest/src/app/rutas/ruta-buscar-libro/ruta-buscar-libro.component.ts
@@ -9,34 +9,42 @@ import {LibroRestService} from "../../services/rest/libro-rest.service";
 })
 export class RutaBuscarLibroComponent implements OnInit {
 
+  libros = [];
+  idUsuario: number;
+
   constructor(
     private readonly _activatedRoute: ActivatedRoute,
     private readonly _libroRestService: LibroRestService,
   ) {
   }
+
   ngOnInit() { // ESTA LISTO EL COMPONENTE SE EJECUTA ESTE CODIGO
     this.escucharParametros();
   }
+
   escucharParametros() {
     const parametrosPapa$ = this._activatedRoute.parent.params;
     parametrosPapa$
       .subscribe(
-        (parametros: {idUsuario:string}) => {
+        (parametros: { idUsuario: string }) => {
           console.log(parametros); // idUsuario
+          this.idUsuario = +parametros.idUsuario;
           this.buscarLibrosPorIdUsuario(+parametros.idUsuario);
         }
       );
   }
+
   buscarLibrosPorIdUsuario(idUsuario: number) {
     const consulta = `idUsuario=${idUsuario}`;
-    const libros$  = this._libroRestService
-              .buscar(consulta);
+    const libros$ = this._libroRestService
+      .buscar(consulta);
     libros$
       .subscribe(
-        (libros)=>{ // OK
+        (libros) => { // OK
           console.log(libros);
+          this.libros = libros;
         },
-        (error)=>{  // ERROR
+        (error) => {  // ERROR
           console.error(error);
         }
       )
